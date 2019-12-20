bfe5a9390691ff6c0b37e0c9627e9b133fce2845
mnichol3 <nicholson.matt94@gmail.com>
2019-12-20 14:14:17 -0500

    Pretty print

--- emissions-freeze/rename_files.py
+++ emissions-freeze/rename_files.py
@@ -1,19 +1,21 @@
 import os
 
 def rename(dir, target_str, replace_str):
-    print("\n...Changing working directory to {}...\n".format(dir))
+    print("\n   Changing working directory to {}...\n".format(dir))
     orig_wd = os.getcwd()
     os.chdir(dir)
     
-    print("...Renaming files...")
+    print("   Renaming files...")
+    
+    longest_str = max([len(x) for x in os.listdir() if target_str in x])
     
     for f in os.listdir():
         if (target_str) in f:
             src = f
             dest = src.replace(target_str, replace_str)
-            print("...{} --> {}".format(src, dest))
+            print("   {} {} {}".format(src, '-->'.rjust(longest_str - len(f) + 3, '-'), dest))
             os.rename(src, dest)
-    print("\n...Finished!\n...Changing working directory back to {}".format(orig_wd))
+    print("\n   Finished!\n   Changing working directory back to {}".format(orig_wd))
     os.chdir(orig_wd)
     
 
@@ -23,6 +25,7 @@ def main():
     replace_str = "frozen"
     
     rename(base_dir, target_str, replace_str)
+    # rename(base_dir, replace_str, target_str)
     
     
 if __name__ == '__main__':
