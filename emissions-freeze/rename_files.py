968eb226c493d0853157862e8e7591e51aa41b02
mnichol3 <nicholson.matt94@gmail.com>
2019-12-23 10:29:51 -0500

    Add CLI

--- emissions-freeze/rename_files.py
+++ emissions-freeze/rename_files.py
@@ -1,4 +1,68 @@
+"""
+Python 3.6
+
+Author: Matt Nicholson
+23 Dec 2019
+
+This script iterates through the files in a given directory and replaces a target
+substring in filenames with a new substring
+
+Example usage
+--------------
+$ python rename_files.py -d 'CEDS\final-emissions\previous-versions' -t '2019_08_25' -n 'frozen'
+
+CEDS_BC_emissions_by_country_sector_v_2019_08_25.csv --> CEDS_BC_emissions_by_country_sector_v_frozen.csv
+"""
 import os
+import argparse
+
+
+
+def init_parser():
+    """
+    Create & return a parser for command line arguments
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    parser : argparse ArgumentParser object
+    
+    Arguments
+    ----------
+        -d, --dir (str)
+            Directory that holds the files to rename
+        -t, --target (str)
+            Target string that we wish to replace
+        -n, --newstr (str)
+            String to replace the target string with
+    """
+    
+    pars_desc = """A script to rename file names by replaceing a target substring
+    with a new substring. Ex: python rename_files.py -d 'CEDS\final-emissions\previous-versions' 
+    -t '2019_08_25' -n 'frozen'"""
+    
+    curr_pwd = os.getcwd()
+    
+    parser = argparse.ArgumentParser(description=parse_desc)
+    
+    parser.add_argument('-d', '--dir', metavar='dir', required=True,
+                        dest='dir', action='store', type=str, default=None,
+                        help='Directory that holds the files to rename')
+                        
+    parser.add_argument('-t', '--target', metavar='target', required=True,
+                        dest='target', action='store', type=str, default=None,
+                        help='Sub-string to replace')
+                        
+    parser.add_argument('-n', '--newstr', metavar='newstr', required=True,
+                        dest='newstr', action='store', type=str, default=None,
+                        help='Replacement sub-string')
+                        
+    return parser
+    
+    
 
 def rename(dir, target_str, replace_str):
     print("\n   Changing working directory to {}...\n".format(dir))
@@ -20,12 +84,15 @@ def rename(dir, target_str, replace_str):
     
 
 def main():
+
+    parser = init_parser()
+    args = parser.parse_args()
+    
     base_dir = r"C:\Users\nich980\code\CEDS\final-emissions\previous-versions"
     target_str = "2019_08_25"
     replace_str = "frozen"
     
     rename(base_dir, target_str, replace_str)
-    # rename(base_dir, replace_str, target_str)
     
     
 if __name__ == '__main__':
