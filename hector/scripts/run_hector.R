d8679bd8358300666f87939564d1b0cf88e30354
mnichol3 <nicholson.matt94@gmail.com>
2020-01-20 13:04:16 -0500

    Config for fetchvars_all testing

--- hector/scripts/run_hector.R
+++ hector/scripts/run_hector.R
@@ -13,12 +13,11 @@ run(core)
 vars <- c(ATMOSPHERIC_CH4(), ATMOSPHERIC_CO2(), ATMOSPHERIC_N2O(),
           RF_CH4(), RF_CO2(), RF_N2O())
 
-rslts <- fetchvars(core, 1745:2300, vars)
+# rslts <- fetchvars(core, 1745:2300, vars)
+rslt <- fetchvars_all(core, 2100:2150, outfile="C:\\Users\\nich980\\data\\hector\\output\\fetchvars_all.csv")
 
-outpath <- "C:/Users/nich980/data/hector/output/nominal_run.csv"
-write.csv(rslts, outpath, row.names = FALSE)
-message( paste0( "Output written to ", outpath ) )
 
-#fetchvars_all(core, 2100:2150)
-# rslt <- fetchvars_all(core, 2100:2150, outpath="C:\\Users\\nich980\\data\\hector\\output")
-# print(rslt)
+
+# outpath <- "C:/Users/nich980/data/hector/output/nominal_run.csv"
+# write.csv(rslts, outpath, row.names = FALSE)
+# message( paste0( "Output written to ", outpath ) )
