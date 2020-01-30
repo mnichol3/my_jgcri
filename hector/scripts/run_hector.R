98d9ce3c6a6e87451de0a8fd29ba4f8de2a3da6b
mnichol3 <nicholson.matt94@gmail.com>
2020-01-30 11:59:34 -0500

    things

--- hector/scripts/run_hector.R
+++ hector/scripts/run_hector.R
@@ -5,17 +5,16 @@ library(hector)
 # ini_file <- system.file("input/gas_paris_med.ini", package = "hector")
 ini_file <- system.file("input/hector_rcp45.ini", package = "hector")
 
-# core <- newcore(ini_file, name="Billy", loglevel = 0, suppresslogging = F)
-core <- newcore(ini_file, name="Billy")
+core <- newcore(ini_file, name="Billy", loglevel = 0, suppresslogging = F)
+# core <- newcore(ini_file, name="Billy")
 
 run(core)
 
-vars <- c(ATMOSPHERIC_CH4(), ATMOSPHERIC_CO2(), ATMOSPHERIC_N2O(),
-          RF_CH4(), RF_CO2(), RF_N2O())
+vars <- c(ATMOSPHERIC_CH4(), ATMOSPHERIC_CO2(), ATMOSPHERIC_N2O())
 
 # rslts <- fetchvars(core, 1745:2300, vars)
-rslt <- fetchvars_all(core, 2100:2150, outfile="C:\\Users\\nich980\\data\\hector\\output\\fetchvars_all.csv")
-
+rslt <- fetchvars(core, 2100:2150, vars)
+rslt
 
 
 # outpath <- "C:/Users/nich980/data/hector/output/nominal_run.csv"
